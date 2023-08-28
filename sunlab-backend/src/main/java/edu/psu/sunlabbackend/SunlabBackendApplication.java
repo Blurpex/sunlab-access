cb6403da4a5aadcc3eb9c6d6c354ecaa031f6df9
Haris Ahmad <harizahmad100@gmail.com>
2023-08-27 23:10:29 -0400

    initil commit

---
+++ sunlab-backend/src/main/java/edu/psu/sunlabbackend/SunlabBackendApplication.java
@@ -0,0 +1,13 @@
+package edu.psu.sunlabbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunlabBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunlabBackendApplication.class, args);
+	}
+
+}
---
+++ sunlab-backend/src/test/java/edu/psu/sunlabbackend/SunlabBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.psu.sunlabbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunlabBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
