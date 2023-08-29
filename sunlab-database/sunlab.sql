b69e270e72812210232ff8c21ff73085ed33cf6c
Haris Ahmad <harizahmad100@gmail.com>
2023-08-28 22:50:43 -0400

    fixed minor issues

--- sunlab-database/sunlab.sql
+++ sunlab-database/sunlab.sql
@@ -2,18 +2,17 @@ DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS log;
 
 CREATE TABLE users (
-    id      BIGINT,
-    name    VARCHAR(255),
-    role    VARCHAR(20) CHECK (role IN ('ADMIN, STUDENT, FACULTY, JANITOR')),
+    id      BIGINT NOT NULL,
+    name    VARCHAR(255) NOT NULL,
+    role    VARCHAR(20) CHECK (role IN ('ADMIN', 'STUDENT', 'FACULTY', 'JANITOR')),
     status  VARCHAR(20) CHECK (status IN ('ACTIVE', 'SUSPENDED')),
     PRIMARY KEY (id)
 );
 
 CREATE TABLE log (
-    id      BIGINT ,
-    time    TIME,
+    id      BIGINT,
+    time    TIMESTAMP NOT NULL,
     location VARCHAR(5) CHECK (location IN ('IN', 'OUT')),
     PRIMARY KEY (id, time),
     FOREIGN KEY (id) REFERENCES users (id)
 );
-
