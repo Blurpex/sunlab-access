5d6ee1a8adb06daf9bdc5a57bd4a3fc6e004751c
Haris Ahmad <harizahmad100@gmail.com>
2023-09-05 19:58:28 -0400

    changed users to user

--- sunlab-database/sunlab.sql
+++ sunlab-database/sunlab.sql
@@ -1,7 +1,7 @@
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS user;
 DROP TABLE IF EXISTS log;
 
-CREATE TABLE users (
+CREATE TABLE user (
     id      BIGINT NOT NULL,
     name    VARCHAR(255) NOT NULL,
     role    VARCHAR(20) CHECK (role IN ('ADMIN', 'STUDENT', 'FACULTY', 'JANITOR')),
@@ -14,5 +14,5 @@ CREATE TABLE log (
     time    TIMESTAMP NOT NULL,
     location VARCHAR(5) CHECK (location IN ('IN', 'OUT')),
     PRIMARY KEY (time),
-    FOREIGN KEY (id) REFERENCES users (id)
+    FOREIGN KEY (id) REFERENCES user (id)
 );
