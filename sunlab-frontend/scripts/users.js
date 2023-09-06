12dfb853a6e9b69e262d0057aa6e21d9a5079c80
Haris Ahmad <harizahmad100@gmail.com>
2023-09-06 19:42:16 -0400

    added all functionalities for users

--- sunlab-frontend/scripts/users.js
+++ sunlab-frontend/scripts/users.js
@@ -1,16 +1,64 @@
+// load all users when the page is initially loaded
 window.onload = () => getAllUsers();
 
+// fill select form with given data
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('select');
     var instances = M.FormSelect.init(elems);
 });
 
-async function getAllUsers() {
-    const url = "http://localhost:8080/users"
+// keep track of current user form data
+let formInput = { id:"", name:"", role:"", status:"" }
+
+// get the current form data
+const getUserFormData = () => {
+    formInput.id = document.getElementById("id").value
+    formInput.name = document.getElementById("name").value
+    formInput.role = document.getElementById("role").value
+    formInput.status = document.getElementById("status").value
+}
+
+// get users from a given url
+async function fetchUsers(url) {
     const response = await fetch(url);
     const data = await response.json();
     let users = data['_embedded']
     displayUsers(users['users'])
+}
+
+// get all users
+function getAllUsers() {
+    const url = "http://localhost:8080/users"
+    fetchUsers(url);
+}
+
+// search users by filters
+const searchUser = () => {
+    getUserFormData();
+
+    let url = "http://localhost:8080/users/search/searchUsers?"
+    Object.keys(formInput).forEach(paramName => {
+        let paramValue = formInput[paramName]
+        if(paramValue !== "") url += `${paramName}=${paramValue}&`
+    })
+    url = url.slice(0, -1);
+    fetchUsers(url);
+}
+
+// add user from given data
+const addUser = () => {
+    getUserFormData()
+    if (formInput.id === "" || formInput.name === "" || formInput.status === "" || formInput.role === "") {
+        alert("Please select all fields!");
+        return;
+    }
+    if (!confirm("Are you sure you want to add this user?")) return;
+
+    fetch("http://localhost:8080/users", {
+        method: "POST",
+        headers: {"Content-type": "application/json"},
+        body: JSON.stringify({id: formInput.id, name: formInput.name, role: formInput.role, status: formInput.status})
+    }).then(() => getAllUsers());
 }
 
 const displayUsers = (users) => {
@@ -25,27 +73,4 @@ const displayUsers = (users) => {
                 <td>${user.status}</td>
             </tr>`;
     })
-}
-
-const addUser = () => {
-
-    if (!confirm("Are you sure you want to add this user?")) return;
-
-    let id = document.getElementById("id").value
-    let name = document.getElementById("name").value
-    let status = document.getElementById("status").value
-    let role = document.getElementById("role").value
-
-    if (!id || !name || !status || !role) {
-        alert("Please select all fields!");
-        return;
-    }
-
-    fetch("http://localhost:8080/users", {
-        method: "POST",
-        headers: {"Content-type": "application/json"},
-        body: JSON.stringify({id: id, name: name, role: role, status: status})
-    }).then(r => getAllUsers());
-
-
 }
