c3d35c714062de97e193d47f78666e432b33e269
Haris Ahmad <harizahmad100@gmail.com>
2023-09-06 19:43:31 -0400

    added custom queries to search users

--- sunlab-backend/src/main/java/edu/psu/sunlabbackend/repository/UsersRepository.java
+++ sunlab-backend/src/main/java/edu/psu/sunlabbackend/repository/UsersRepository.java
@@ -2,5 +2,18 @@ package edu.psu.sunlabbackend.repository;
 
 import edu.psu.sunlabbackend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface UsersRepository extends JpaRepository<User, Long> {}
+import java.util.List;
+
+public interface UsersRepository extends JpaRepository<User, Long> {
+
+    @Query(value = "SELECT * FROM user " +
+        "WHERE (LOWER(user.name) LIKE CONCAT('%', :name, '%') OR :name IS NULL) " +
+        "AND (id = :id OR :id IS NULL) " +
+        "AND (role = :role OR :role IS NULL) " +
+        "AND (status = :status OR :status IS NULL)"
+        , nativeQuery = true)
+    List<User> searchUsers(String name, Long id, String role, String status);
+
+}
