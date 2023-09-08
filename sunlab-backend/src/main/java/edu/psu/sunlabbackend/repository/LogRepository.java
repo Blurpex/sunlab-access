88fc33f52e4ed070003260821ad1ede5880c3e0c
Haris Ahmad <harizahmad100@gmail.com>
2023-09-07 23:24:19 -0400

    added custom query search for log

--- sunlab-backend/src/main/java/edu/psu/sunlabbackend/repository/LogRepository.java
+++ sunlab-backend/src/main/java/edu/psu/sunlabbackend/repository/LogRepository.java
@@ -2,7 +2,18 @@ package edu.psu.sunlabbackend.repository;
 
 import edu.psu.sunlabbackend.model.Log;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.sql.Timestamp;
+import java.util.List;
 
-public interface LogRepository extends JpaRepository<Log, Timestamp> {}
+public interface LogRepository extends JpaRepository<Log, Timestamp> {
+
+    @Query(value = "SELECT * FROM log " +
+            "WHERE (id = :id OR :id IS NULL) " +
+            "AND (location = :location OR :location IS NULL) " +
+            "AND (time = :time OR :time IS NULL)"
+            , nativeQuery = true)
+    List<Log> searchLogs(Long id, String location, Timestamp time);
+
+}
